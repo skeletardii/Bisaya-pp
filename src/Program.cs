39be29f0f9eb3edd3075c411237084d3b9ced8b4
cusgan <fmfgbascug@gmail.com>
2025-05-07 03:15:13 +0800

    Add code for testing Lexer in Program

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("hello baby");
+﻿using Bisaya__.src.Core;
+string filePath = Path.Combine("tests","sample.txt");
+string content = File.ReadAllText("..\\..\\..\\tests\\testcases\\test9.bpp");
+
+static string padright(string s, int width)
+{
+    for (int i = s.Length; i < width; i++)
+    {
+        s += " ";
+    }
+    return s;
+}
+// See list of tokens in program
+List<List<Token>> tokens = Lexer.tokenize(content);
+foreach (List<Token> line in tokens)
+{
+    foreach (Token token in line)
+    {
+        Console.WriteLine($"{padright(token.Type.ToString(),20)} \t {padright(token.Value.ToString(), 10)} \t Line: {token.LineNumber} \t Col: {token.ColumnNumber}");
+    }
+}
